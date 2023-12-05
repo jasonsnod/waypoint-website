57b32ac6b467f0c9eb6deffdf9d0131a14432dff
Jason Snoddy <jasonsnod@gmail.com>
2023-12-05 09:27:32 -0500

    JS: Created Image, Review (models)

---
+++ java/src/main/java/com/techelevator/model/Image.java
@@ -0,0 +1,5 @@
+package com.techelevator.model;
+
+// TODO: Might need to make corresponding Dto for Image (under review)
+public class Image {
+}
--- java/src/main/java/com/techelevator/model/Landmark.java
+++ java/src/main/java/com/techelevator/model/Landmark.java
@@ -1,4 +1,12 @@
 package com.techelevator.model;
 
 public class Landmark {
+
+    private int landmarkId;
+    private String landmarkName;
+    private String landmarkAddress;
+    private String landmarkDetails;
+    List<Image> landmarkImages;
+    List<Review> landmarkReviews;
+
 }
