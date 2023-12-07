855cdf55eae7ee24daa35a09b91096f08dfa620b
ashton8504 <ashtonlittle8504@gmail.com>
2023-12-07 09:52:14 -0500

    added a router link to LandmarksView

--- vue/src/views/LandmarksView.vue
+++ vue/src/views/LandmarksView.vue
@@ -1,60 +1,62 @@
 <template>
-    <global-header />
-    <landmark-search />  
-    <map-of-landmarks />  
-    <div class="landmark-card-list" v-for="landmark in landmarks" v-bind:key="landmark.landmarkId">
-        <landmark-card v-bind:landmark="landmark"/>
-    </div>
-    <global-footer />
+  <global-header />
+  <landmark-search />
+  <map-of-landmarks />
+  <div
+    class="landmark-card-list"
+    v-for="landmark in landmarks"
+    v-bind:key="landmark.landmarkId"
+  >
+    <landmark-card v-bind:landmark="landmark" />
+  </div>
+  <router-link to="/landmarks"> Explore Landmarks</router-link>
+  <global-footer />
 </template>
 
 <script>
-import GlobalHeader from '../components/GlobalHeader.vue';
-import GlobalFooter from '../components/GlobalFooter.vue';
-import LandmarkSearch from '../components/LandmarkSearch.vue';
-import MapOfLandmarks from '../components/MapOfLandmarks.vue';
-import LandmarkCard from '../components/LandmarkCard.vue'
-import landmarkService from '../services/LandmarkService';
+import GlobalHeader from "../components/GlobalHeader.vue";
+import GlobalFooter from "../components/GlobalFooter.vue";
+import LandmarkSearch from "../components/LandmarkSearch.vue";
+import MapOfLandmarks from "../components/MapOfLandmarks.vue";
+import LandmarkCard from "../components/LandmarkCard.vue";
+import landmarkService from "../services/LandmarkService";
 
 export default {
-    data() {
-        return {
-            landmarks: []
-        }
+  data() {
+    return {
+      landmarks: [],
+    };
+  },
+  components: {
+    GlobalHeader,
+    GlobalFooter,
+    LandmarkSearch,
+    MapOfLandmarks,
+    LandmarkCard,
+  },
+  method: {
+    getLandmarks() {
+      landmarkService
+        .getAllLandmarks()
+        .then((response) => {
+          this.landmarks = response.data;
+        })
+        .catch((error) => {
+          this.handleErrorResponse(error);
+        });
     },
-    components: {
-        GlobalHeader,
-        GlobalFooter,
-        LandmarkSearch,
-        MapOfLandmarks,
-        LandmarkCard
+    handleErrorResponse(error) {
+      if (error.response.status == 404) {
+        alert(`${error.response.status} + No landmarks found`);
+      } else {
+        alert(`An unknown error occurred while searching for landmarks.`);
+      }
     },
-    method: {
-        getLandmarks() {
-            landmarkService.getAllLandmarks()
-                .then(response => {
-                    this.landmarks = response.data;
-                })
-                .catch(error => {
-                    this.handleErrorResponse(error);
-                })
-        },
-        handleErrorResponse(error) {
-            if (error.response.status == 404) {
-                alert(`${error.response.status} + No landmarks found`);
-            }
-            else {
-                alert(`An unknown error occurred while searching for landmarks.`)
-            }
-        }
-    },
-    created() {
-        this.getLandmarks();
-    }
-}
-
+  },
+  created() {
+    this.getLandmarks();
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
