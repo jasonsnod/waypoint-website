89ba870b2d3b1ede34d91adbe5792ea56f45a9e6
ashton8504 <ashtonlittle8504@gmail.com>
2023-12-07 07:04:30 -0500

    saving work on HomeView

--- vue/src/components/GlobalFooter.vue
+++ vue/src/components/GlobalFooter.vue
@@ -1,55 +1,57 @@
 <template>
-<<<<<<< HEAD
-  
   <footer class="bg-white fixed-bottom py-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="footer-column">
-                        <ul>
-                          <h6>Explore</h6>
-                              <li><router-link :to="{ name: 'home' }">Home</router-link></li>
-                              <li><a href="#">About</a></li>
-                              <li><a href="#">FAQ</a></li>
-                              <li><a href="#">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-                  <div class="col-md-4">
-                      <div class="footer-column">
-                        <ul>
-                          <h6>More</h6>
-                              <li><a href="#">Create/View Itineraries</a></li>
-                              <li><a href="#">View Landmarks</a></li>
-                              <li v-show="isAdmin"><a href="#">View Landmarks</a></li>
-                        </ul>
-                      </div>
-                  </div>
-                <div class="col-md-4">
-                    <div class="footer-column">
-                      <ul>
-                          <h6>Contact</h6>
-                              <span>Phone Number: 855-867-5309</span>
-                              <li><span>Email: <a href="mailto:your@email.com">youshould@notemailus.com</a></span></li>
-                              <span>Mailing Address: </span>
-                              <li><p>
-                                      <span class="address-line">Waypoint, 123 Main Street</span><br>
-                                      <span class="address-line">Cityville, State, USA 12345</span><br>
-                                  </p></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="footer-column">
+            <ul>
+              <h6>Explore</h6>
+              <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
         </div>
-<span id="waypoint" class="text-black">&copy; 2023 Waypoint</span>
-
-=======
-  <div>
-    <footer id="footer" class="bg-body-tertiary text-center p-3 mt-5">
-      <p class="footer">City Tours Est 2023</p>
->>>>>>> b3a26c5 (saving work on homepage and what i stated i was working on)
-    </footer>
-
+        <div class="col-md-4">
+          <div class="footer-column">
+            <ul>
+              <h6>More</h6>
+              <li><a href="#">Create/View Itineraries</a></li>
+              <li><a href="#">View Landmarks</a></li>
+              <li v-show="isAdmin"><a href="#">View Landmarks</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="footer-column">
+            <ul>
+              <h6>Contact</h6>
+              <span>Phone Number: 855-867-5309</span>
+              <li>
+                <span
+                  >Email:
+                  <a href="mailto:your@email.com"
+                    >youshould@notemailus.com</a
+                  ></span
+                >
+              </li>
+              <span>Mailing Address: </span>
+              <li>
+                <p>
+                  <span class="address-line">Waypoint, 123 Main Street</span
+                  ><br />
+                  <span class="address-line">Cityville, State, USA 12345</span
+                  ><br />
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <span id="waypoint" class="text-black">&copy; 2023 Waypoint</span>
+  </footer>
 </template>
 
 <script>
@@ -58,20 +60,17 @@ export default {
     return {
       user: {
         authenticated: true, // Add actual properties to match authentication and admin rights
-        isAdmin: true 
-        
-      }
+        isAdmin: true,
+      },
     };
   },
   computed: {
     isAdminUser() {
       // Add actual properties to match authentication and admin rights
       return this.user.authenticated && this.user.isAdmin;
-    }
-  }
+    },
+  },
 };
-
-
 </script>
 
 <style scoped>
@@ -83,15 +82,13 @@ export default {
 
 a {
   text-decoration: none;
-  color: #1D366F;
+  color: #1d366f;
   transition: color 0.3s ease;
-
 }
 
 a:hover {
   text-decoration: underline;
-  color: #08C076
-  
+  color: #08c076;
 }
 
 ul {
@@ -101,9 +98,6 @@ ul {
 footer {
   margin-top: auto;
   padding: 20;
-  border-top: 1px solid #1D366F;
+  border-top: 1px solid #1d366f;
 }
-
-
 </style>
-
