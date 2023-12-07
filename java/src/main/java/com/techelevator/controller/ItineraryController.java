0bb499f323f2672ad97a026397a5b651ae2c6d26
Zach Huster <huster.8@gmail.com>
2023-12-07 16:50:45 -0500

    Merge branch 'main' of https://git.techelevator.com/campuses/nlr/sep-2023/java-blue/student-pairs/final-capstone/capstone-team-4
    # Conflicts:
    #	vue/img/itinerary-logo.png
    #	vue/img/landmarks-logo.png
    #	vue/img/logo-large.png
    #	vue/img/logo-small.png

---
+++ java/src/main/java/com/techelevator/controller/ItineraryController.java
@@ -0,0 +1,57 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.ItineraryDao;
+import com.techelevator.dao.JdbcItineraryDao;
+import com.techelevator.exception.DaoException;
+import com.techelevator.model.Itinerary;
+import com.techelevator.model.LandmarkDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/itinerary")
+// @PreAuthorize("isAuthenticated")
+public class ItineraryController {
+
+    private ItineraryDao itineraryDao;
+
+    public ItineraryController(ItineraryDao itineraryDao) {
+        this.itineraryDao = itineraryDao;
+    }
+
+    @RequestMapping(method= RequestMethod.GET)
+    public List<Itinerary> getItineraries(int userId) {
+        return itineraryDao.getItinerariesByUser(userId);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(method = RequestMethod.POST)
+    public Itinerary addNewItinerary(@Valid @RequestBody Itinerary newItinerary) {
+        //TODO: Possibly need to Link Itinerary/Landmark Table to add landmarks
+        try {
+            return itineraryDao.createItinerary(newItinerary);
+        } catch(DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(path = "/{itineraryId}", method = RequestMethod.PUT)
+    public Itinerary editItinerary(@PathVariable int itineraryId, @RequestBody Itinerary dataItinerary) {
+        //TODO: Decide on and Implement a way to alter landmarks in an itinerary
+        try {
+            return itineraryDao.updateItinerary(dataItinerary);
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(path = "/{itineraryId}", method = RequestMethod.DELETE)
+    public void deleteItinerary(@PathVariable int itineraryId) {
+        itineraryDao.deleteItinerary(itineraryId);
+    }
+}
---
+++ java/src/main/java/com/techelevator/dao/JdbcItineraryDao.java
@@ -0,0 +1,125 @@
+package com.techelevator.dao;
+
+import com.techelevator.exception.DaoException;
+import com.techelevator.model.Itinerary;
+import com.techelevator.model.LandmarkDto;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcItineraryDao implements ItineraryDao{
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcItineraryDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Itinerary> getItinerariesByUser(int userId) {
+        List<Itinerary> listOfUsersItineraries = new ArrayList<>();
+
+        String sqlSelectQuery = (
+                "SELECT itinerary_id, user_id, itinerary_name, starting_address \n" +
+                        "FROM itineraries " +
+                        "WHERE user_id = ?"
+        );
+
+        try {
+            SqlRowSet resultsFromQuery = jdbcTemplate.queryForRowSet(sqlSelectQuery, userId);
+            while(resultsFromQuery.next()) {
+                listOfUsersItineraries.add(mapRowToItinerary(resultsFromQuery));
+            }
+        }
+        catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+
+        return listOfUsersItineraries;
+    }
+
+    @Override
+    public Itinerary getItineraryById(int itineraryId) {
+        Itinerary useItinerary = null;
+        String sqlQuery = "SELECT itinerary_id, user_id, itinerary_name, starting_address " +
+                            "FROM itineraries " +
+                            "WHERE itinerary_id = ?";
+
+        try {
+            SqlRowSet resultsFromQuery = jdbcTemplate.queryForRowSet(sqlQuery, itineraryId);
+            if(resultsFromQuery.next()) {
+                useItinerary = mapRowToItinerary(resultsFromQuery);
+            }
+        }
+        catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+
+        return useItinerary;
+    }
+
+    @Override
+    public Itinerary createItinerary(Itinerary newItinerary) {
+        Itinerary createdItinerary = null;
+        String sqlQuery = "INSERT INTO itineraries (user_id, itinerary_name, starting_address) " +
+                            "VALUES (?,?,?) " +
+                            "RETURNING itinerary_id";
+
+        try {
+            int createdItineraryId = jdbcTemplate.queryForObject(
+                    sqlQuery,
+                    int.class,
+                    newItinerary.getCreatorId(),
+                    newItinerary.getItineraryName(),
+                    newItinerary.getStartingAddress()
+            );
+            createdItinerary = getItineraryById(createdItineraryId);
+        }
+        catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        catch(DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+        return createdItinerary;
+    }
+
+    @Override
+    public Itinerary updateItinerary(Itinerary dataItinerary) {
+        Itinerary updatedItinerary = null;
+        String sqlQuery = "UPDATE itineraries SET itinerary_name = ?, starting_address = ? WHERE itinerary_id = ?";
+        try {
+            int rowsAffected = jdbcTemplate.update(sqlQuery, dataItinerary.getItineraryName(), dataItinerary.getStartingAddress(), dataItinerary.getItineraryId());
+            if (rowsAffected == 0) {
+                throw new DaoException("Nothing was changed.");
+            }
+            updatedItinerary = getItineraryById(dataItinerary.getItineraryId());
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+        return updatedItinerary;
+    }
+
+    @Override
+    public void deleteItinerary(int itineraryId) {
+        // TODO: Maybe link the itinerary/landmark table here too?
+        String sqlQuery = "DELETE * FROM itineraries WHERE itinerary_id = ?";
+        jdbcTemplate.update(sqlQuery, itineraryId);
+    }
+
+    public Itinerary mapRowToItinerary(SqlRowSet resultsToMap){
+        Itinerary itineraryToCreate = new Itinerary();
+
+        itineraryToCreate.setItineraryId(resultsToMap.getInt("itinerary_id"));
+        itineraryToCreate.setCreatorId(resultsToMap.getInt("user_id"));
+        itineraryToCreate.setItineraryName(resultsToMap.getString("itinerary_name"));
+        itineraryToCreate.setStartingAddress(resultsToMap.getString("starting_address"));
+
+        return itineraryToCreate;
+    }
+}
