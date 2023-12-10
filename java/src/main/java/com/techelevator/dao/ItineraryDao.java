2bcdb373905866c049c16d9664271b3d74c524dc
David Boyd <davidboyd115@gmail.com>
2023-12-10 17:57:33 -0500

    DB: Added route to get landmarks by itinerary

--- java/src/main/java/com/techelevator/dao/ItineraryDao.java
+++ java/src/main/java/com/techelevator/dao/ItineraryDao.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface ItineraryDao {
     List<Itinerary> getItinerariesByUser(int userId);
     Itinerary getItineraryById(int itineraryId);
+    List<Integer> getItineraryLandmarks(int itineraryId);
     Itinerary createItinerary(Itinerary newItinerary);
     Itinerary addItineraryLandmarks(int itineraryId, List<Integer> landmarkIds);
     Itinerary updateItinerary(Itinerary dataItinerary);
