8f2a105a8b0e0df826108564c8882bcc27220b32
ashton8504 <ashtonlittle8504@gmail.com>
2023-12-10 12:23:38 -0500

    AL: added style tag to map component

--- vue/src/components/MapOfLandmarks.vue
+++ vue/src/components/MapOfLandmarks.vue
@@ -1,19 +1,20 @@
 <template>
-    <h1>Map of Landmarks Test</h1>
-    <!-- <img width="600" 
+  <h1>Map of Landmarks Test</h1>
+  <!-- <img width="600" 
          height="400" 
          src="../../img/all-landmarks-static-map.jpg"
          alt="1725 Riverside Drive, Cincinnati, OH 45202, United States of America"
     /> -->
-    <img width="600" height="400" 
-src="https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=400&center=lonlat:-84.482038,39.116247&zoom=11.4604&marker=lonlat:-84.5082596024472,39.07830587854244;color:%23ff0000;size:small|lonlat:-84.52642222209342,39.08815258064968;color:%23ff0000;size:small|lonlat:-84.42370410995925,39.1411755209522;color:%23ff0000;size:small&apiKey=f4ec423be4f84260bdd3b93a488dd3b0"
-alt="1725 Riverside Drive, Cincinnati, OH 45202, United States of America">
-
+  <img
+    width="600"
+    height="400"
+    src="https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=400&center=lonlat:-84.482038,39.116247&zoom=11.4604&marker=lonlat:-84.5082596024472,39.07830587854244;color:%23ff0000;size:small|lonlat:-84.52642222209342,39.08815258064968;color:%23ff0000;size:small|lonlat:-84.42370410995925,39.1411755209522;color:%23ff0000;size:small&apiKey=f4ec423be4f84260bdd3b93a488dd3b0"
+    alt="1725 Riverside Drive, Cincinnati, OH 45202, United States of America"
+  />
 </template>
 
 <script>
-
-export default {
-
-}
+export default {};
 </script>
+
+<styled scoped></styled>
