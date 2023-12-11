a33b30c9e55a396d7f5db841930d086d3767abb4
David Boyd <davidboyd115@gmail.com>
2023-12-11 15:22:41 -0500

    DB: Added a Button

--- vue/src/views/ItinerariesView.vue
+++ vue/src/views/ItinerariesView.vue
@@ -4,6 +4,7 @@
     <div class="itineraries-list" v-for="itinerary in this.$store.state.itineraries" v-bind:key="itinerary.itineraryId">
         <itinerary-card v-bind:itinerary="itinerary"/>
     </div>
+    <button class="new-itinerary-button">Create New Itinerary</button>
     <global-footer />
 </template>
 
