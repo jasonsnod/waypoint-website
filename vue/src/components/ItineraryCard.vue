819137229176184e5b851f56a90cdb50db130329
Jason Snoddy <jasonsnod@gmail.com>
2023-12-11 17:29:46 -0500

    JS: Step over

--- vue/src/components/ItineraryCard.vue
+++ vue/src/components/ItineraryCard.vue
@@ -5,7 +5,6 @@
     <p>{{ itinerary.itineraryId}}</p>
     <p>{{ itinerary.userId }}</p>
     <p>{{ itinerary.startingAddress }}</p>
-    <!-- <router-link v-bind="{ name: 'landmark-details', params: {landmarkId: landmark.landmarkId } }">Link to a LandmarkDetailsView</router-link> -->
 </template>
 
 <script>
