e7e18006de0581a4b97742c44ed4e4ffb6fde957
David Boyd <davidboyd115@gmail.com>
2023-12-11 14:48:33 -0500

    DB: Added methods in Vue to match Back-End

--- vue/src/services/ItineraryService.js
+++ vue/src/services/ItineraryService.js
@@ -3,5 +3,23 @@ import axios from 'axios';
 export default {
     getAllItineraries(userId) {
         return axios.get(`/itinerary/${userId}`);
+    },
+    editLandmarksForItinerary(itineraryId, landmarkIdArray) {
+        let landmarkIdString = landmarkIdArray.join(',')
+        return axios.put(`/itinerary/${itineraryId}/landmark?landmarkIds=${landmarkIdString}`)
+    },
+    createLandmarksForItinerary(itineraryId, landmarkIdArray) {
+        let landmarkIdString = landmarkIdArray.join(',')
+        return axios.post(`/itinerary/${itineraryId}/landmark?landmarkIds=${landmarkIdString}`)
+    },
+    deleteItinerary(itineraryId) {
+        return axios.delete(`/itinerary/${itineraryId}`)
+    },
+    createBaseItinerary() {
+        return axios.post('/itinerary')
+    },
+    updateItineraryDetails(itineraryId) {
+        return axios.put(`/itinerary/${itineraryId}`)
     }
+
 }
