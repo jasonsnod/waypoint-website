0fce30c213f888aaa1a4f00ac2efe4ea7a193d48
David Boyd <davidboyd115@gmail.com>
2023-12-12 14:33:14 -0500

    DB: Working Form that Won't Submit

--- vue/src/views/ItinerariesView.vue
+++ vue/src/views/ItinerariesView.vue
@@ -7,14 +7,16 @@
     <button class="new-itinerary-button" v-if="!showForm" v-on:click="flipForm">Create New Itinerary</button>
     <form v-if="showForm" v-on:submit.prevent="createItinerary">
         <p>Itinerary Name</p>
-        <input v-model="itinerary.itineraryName"/>
+        <input v-model="itinerary.itineraryName" id="itinerary-name-input"/>
         <p>Starting Address</p>
-        <input v-model="itinerary.startingAddress"/>
-        <ul class="landmarks-list" v-for="landmark in this.$store.state.landmarks" v-bind:key="landmark.landmarkId">
-            <input type="checkbox" v-bind="landmark.landmarkId" v-on:click="updateIds">{{ landmark.landmarkName }}
+        <input v-model="itinerary.startingAddress" id="starting-address-input"/>
+        <ul class="landmarks-list" >
+            <li v-for="landmark in this.$store.state.landmarks" v-bind:key="landmark.landmarkId">
+                <input type="checkbox" v-on:click="updateIds(landmark.landmarkId)" class="landmark-checkbox" />{{ landmark.landmarkName }}
+            </li>
         </ul>
         
-        <button type="submit" v-on:click="flipForm">Ahhh</button>
+        <button type="submit" v-on:click="flipForm">Submit</button>
 
     </form>
     <global-footer />
@@ -47,11 +49,15 @@ export default {
         flipForm() {
             this.showForm = !this.showForm
         },
-        updateIds() {
-            if (this.landmarkIds.includes(this.landmarkId)) {
-                this.landmarkIds.splice(this.landmarkIds.findIndex(this.landmarkId), 1)
+        updateIds(landmarkId) {
+            console.log(landmarkId)
+            if (this.landmarkIds.includes(landmarkId) === true) {
+                //this.landmarkIds.splice(this.landmarkIds.indexOf(landmarkId), 1)
+                this.landmarkIds = this.landmarkIds.filter(id => id != landmarkId)
+                console.log(this.landmarkIds)
             } else {
-                this.landmarkIds.push(this.landmarkId)
+                this.landmarkIds.push(landmarkId)
+                console.log(this.landmarkIds)
             }
         },
         getItineraries() {
@@ -65,7 +71,13 @@ export default {
         },
         createItinerary() {
             itineraryService.createBaseItinerary(this.itinerary)
-            itineraryService.createLandmarksForItinerary(this.landmarkIds)
+            .then(response => {
+                this.$store.state.itineraries.add(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            // itineraryService.createLandmarksForItinerary(this.landmarkIds)
         },
         handleErrorResponse() {
             console.log('Error: Network Error');
