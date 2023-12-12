0d28a019065ebd93e414a7d38507ccfdfa3d16ad
Jason Snoddy <jasonsnod@gmail.com>
2023-12-12 18:01:13 -0500

    JS: Polished ItineraryService.js

--- vue/src/services/ItineraryService.js
+++ vue/src/services/ItineraryService.js
@@ -4,29 +4,30 @@ export default {
     getAllItineraries(userId) {
         return axios.get(`/itinerary/${userId}`);
     },
+    getLandmarksByItinerary(itineraryId) {
+        return axios.get(`/itinerary/${itineraryId}/landmark`);
+    },
     editLandmarksForItinerary(itineraryId, landmarkIdArray) {
         // let landmarkIdString = landmarkIdArray.join(',')
         //?landmarkIds=${landmarkIdString}
-        return axios.put(`/itinerary/${itineraryId}/landmark`, landmarkIdArray)
+        return axios.put(`/itinerary/${itineraryId}/landmark`, landmarkIdArray);
     },
     createLandmarksForItinerary(itineraryId, landmarkIdArray) {
-        console.log(landmarkIdArray)
-        let landmarkIdString = landmarkIdArray.join(',')
+        console.log(landmarkIdArray);
+        let landmarkIdString = landmarkIdArray.join(',');
         //?landmarkIds=${landmarkIdString}
-        console.log(landmarkIdString)
-        return axios.post(`/itinerary/${itineraryId}/landmark?landmarkIds=${landmarkIdString}`)
+        console.log(landmarkIdString);
+        return axios.post(`/itinerary/${itineraryId}/landmark?landmarkIds=${landmarkIdString}`);
     },
     deleteItinerary(itineraryId) {
-        return axios.delete(`/itinerary/${itineraryId}`)
+        return axios.delete(`/itinerary/${itineraryId}`);
     },
     createBaseItinerary(itinerary) {
-        return axios.post('/itinerary', itinerary)
+        return axios.post('/itinerary', itinerary);
     },
     updateItineraryDetails(itineraryId, itinerary) {
-        return axios.put(`/itinerary/${itineraryId}`, itinerary)
+        return axios.put(`/itinerary/${itineraryId}`, itinerary);
     },
-    getLandmarksByItinerary(itineraryId) {
-        return axios.get(`/itinerary/${itineraryId}/landmark`)
-    }
+
 
 }
